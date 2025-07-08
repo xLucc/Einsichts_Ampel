7adb5996f876e2b6b796eda26b84fea894f73943
Fabian <fabian@pop-os.fritz.box>
2025-07-08 11:22:49 +0200

    changed the protcol

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,100 @@
 #include <Arduino.h>
-#include <SPI.h>
 #include <Adafruit_PN532.h>
 
-// PN532 SPI Pins
-#define PN532_SCK 12
-#define PN532_MOSI 20
-#define PN532_MISO 8
-#define PN532_SS 5
+// Pin setup
 
-// IRQ interrupt pin to ESP32
-#define PN532_IRQ 4
+#define I2C_SDA 8
+#define I2C_SCL 9
+#define PN532_IRQ 10
+#define PN532_RESET -1
 
-Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
+Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
 
-volatile bool nfcInterrupt = false;
-uint8_t payload[32];
-uint8_t payloadLen = 0;
+volatile bool interrupt = false;
+uint8_t KEY_A[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
-void IRAM_ATTR onNfcIRQ()
-{
-  nfcInterrupt = true;
-}
+// Interrupt
 
-// put function declarations here:
-int myFunction(int, int);
+void IRAM_ATTR irqHandler() { interrupt = true; }
 
 void setup()
 {
   Serial.begin(115200);
-  pinMode(PN532_IRQ, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(PN532_IRQ), onNfcIRQ, FALLING);
+  delay(2000);
 
+  Wire.begin(I2C_SDA, I2C_SCL, 400000); // I2C @ 400kHz
   nfc.begin();
-  uint32_t versiondata = nfc.getFirmwareVersion();
-  if (!versiondata)
-  {
-    Serial.println("Did not found PN532");
-    while (1)
-      ;
-  }
   nfc.SAMConfig();
-  Serial.println('NFC ready');
+
+  pinMode(PN532_IRQ, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(PN532_IRQ), irqHandler, FALLING);
+
+  Serial.println('Waiting for Commands.');
 }
 
 void loop()
 {
-  if (!nfcInterrupt)
-    return;
-  nfcInterrupt = false;
-
-  uint8_t uid[7];
-  uint8_t uidLen;
-  if (nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLen, 1000))
+  if (interrupt)
   {
-    payloadLen = uidLen;
-    memcpy(payload, uid, payloadLen);
-
-    Serial.print('Payload:');
-    for (uint8_t i = 0; i < payloadLen; i++)
-    {
-      Serial.print(payload[i], HEX);
-      Serial.print(' ');
-    }
-    Serial.println();
+    interrupt = false;
+    dumpTag();
   }
-  else
+}
+
+void dumpTag()
+{
+  uint8_t uid[7], uidLen;
+
+  if (!nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLen, 0))
   {
-    Serial.println('Reading error or no tag');
+    return;
+  }
+
+  // Read the UID.
+  Serial.printf("\nUID: ");
+  for (uint8_t i = 0; i < uidLen; i++)
+  {
+    if (uid[i] < 0x10)
+    {
+      Serial.print('0');
+    }
+    Serial.print(uid[i], HEX);
+    Serial.print(' ');
+  }
+
+  Serial.println();
+
+  uint8_t data[16];
+
+  // Read all the Blocks.
+  for (uint8_t block = 0; block < 64; block++)
+  {
+    // Authenticate the Block.
+    if (block % 4 == 0)
+    {
+      if (!nfc.mifareclassic_AuthenticateBlock(uid, uidLen, block, 0, KEY_A))
+      {
+        Serial.printf("Auth-Error Sector %u\n", block / 4);
+        return;
+      }
+    }
+
+    // Read the Block
+    if (nfc.mifareclassic_ReadDataBlock(block, data))
+    {
+      Serial.printf("Block %02u: ", block);
+      for (uint8_t i = 0; i < 16; i++)
+      {
+        if (data[i] < 0x10)
+          Serial.print('0');
+        Serial.print(data[i], HEX);
+        Serial.print(' ');
+      }
+      Serial.println();
+    }
+    else
+    {
+      Serial.printf("Reading-Error Block %u\n", block);
+    }
   }
 }

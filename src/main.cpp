20d3b9dbde94e4a2336f4ccb29b1d2fcdae76583
Fabian <fabian@pop-os.fritz.box>
2025-07-09 16:16:58 +0200

    changed to LED

--- src/main.cpp
+++ src/main.cpp
@@ -3,33 +3,51 @@
 
 // Pin setup
 
-#define I2C_SDA 8
-#define I2C_SCL 9
-#define PN532_IRQ 10
+#define I2C_SDA 6
+#define I2C_SCL 7
+#define PN532_IRQ 5
 #define PN532_RESET -1
 
 Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
 
 volatile bool interrupt = false;
 uint8_t KEY_A[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+uint8_t data[4][16];
 
 // Interrupt
 
-void IRAM_ATTR irqHandler() { interrupt = true; }
+void IRAM_ATTR
+irqHandler()
+{
+  interrupt = true;
+}
 
 void setup()
 {
   Serial.begin(115200);
-  delay(2000);
+  Serial.println("NFC Reader init...");
 
-  Wire.begin(I2C_SDA, I2C_SCL, 400000); // I2C @ 400kHz
+  // Initialize the reader
   nfc.begin();
+
+  // Infinite loop if an error occur.
+  if (!nfc.getFirmwareVersion())
+  {
+    Serial.println("Connection-Error");
+    while (1)
+      ;
+  }
+
+  // Start the reader.
   nfc.SAMConfig();
 
+  // Set the interrupt pin.
   pinMode(PN532_IRQ, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(PN532_IRQ), irqHandler, FALLING);
 
-  Serial.println('Waiting for Commands.');
+  pinMode(8, OUTPUT);
+
+  Serial.println("Waiting for cards.");
 }
 
 void loop()
@@ -37,64 +55,22 @@ void loop()
   if (interrupt)
   {
     interrupt = false;
-    dumpTag();
-  }
-}
+    uint8_t uid[7], uidLen;
 
-void dumpTag()
-{
-  uint8_t uid[7], uidLen;
+    // Check the uid, if it's valid.
+    if (!nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLen, 0))
+      return;
 
-  if (!nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLen, 0))
-  {
-    return;
-  }
-
-  // Read the UID.
-  Serial.printf("\nUID: ");
-  for (uint8_t i = 0; i < uidLen; i++)
-  {
-    if (uid[i] < 0x10)
+    // Convert the uid into a string.
+    String uidStr = "";
+    for (uint8_t i = 0; i < uidLen; i++)
     {
-      Serial.print('0');
-    }
-    Serial.print(uid[i], HEX);
-    Serial.print(' ');
-  }
+      if (uid[i] < 0X10)
+        uidStr += "0";
 
-  Serial.println();
-
-  uint8_t data[16];
-
-  // Read all the Blocks.
-  for (uint8_t block = 0; block < 64; block++)
-  {
-    // Authenticate the Block.
-    if (block % 4 == 0)
-    {
-      if (!nfc.mifareclassic_AuthenticateBlock(uid, uidLen, block, 0, KEY_A))
-      {
-        Serial.printf("Auth-Error Sector %u\n", block / 4);
-        return;
-      }
+      uidStr += String(uid[i], HEX);
     }
 
-    // Read the Block
-    if (nfc.mifareclassic_ReadDataBlock(block, data))
-    {
-      Serial.printf("Block %02u: ", block);
-      for (uint8_t i = 0; i < 16; i++)
-      {
-        if (data[i] < 0x10)
-          Serial.print('0');
-        Serial.print(data[i], HEX);
-        Serial.print(' ');
-      }
-      Serial.println();
-    }
-    else
-    {
-      Serial.printf("Reading-Error Block %u\n", block);
-    }
+    digitalWrite(8, HIGH);
   }
 }
